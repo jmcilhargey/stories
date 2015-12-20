f14d18f35e8d51f388370230b98bf3cdb1bd0149
jmcilhargey <jmcilhargey@gmail.com>
2015-12-20 19:33:19 +0000

    Added jQuery Slider for Menu

---
+++ app/assets/javascripts/slider.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    
+    $('.menu-toggle').on('click', function(event){
+    	event.preventDefault();
+    	
+    	// create menu variables
+    	
+    	var slideoutMenu = $('.menu');
+    	var slideoutMenuWidth = $('.menu').width();
+    	
+    	// toggle open class
+    	
+    	slideoutMenu.toggleClass("open");
+    	
+    	// slide menu
+    	
+    	if (slideoutMenu.hasClass("open")) {
+	    	slideoutMenu.animate({
+		    	left: "0px"
+	    	});	
+    	} else {
+	    	slideoutMenu.animate({
+		    	left: -slideoutMenuWidth
+	    	}, 250);	
+    	}
+    	
+    });
+});
