13ccbe301e01d476b800027fbc64a04401d3dcd4
jmcilhargey <jmcilhargey@gmail.com>
2015-12-21 06:19:10 +0000

    Added signup and thanks pages. Added controller action for saving form info to database.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   get '/about' => 'pages#about'
+  get '/signups/new' => 'signups#new'
+  post '/signups' => 'signups#create'
+  get '/thanks' => 'signups#thanks'
+  
+  resources :signups
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
